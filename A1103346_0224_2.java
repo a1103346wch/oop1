85968e8fc2c39eb73da893c89cfecd59ded2db23
chunghao <125645173+a1103346wch@users.noreply.github.com>
2023-02-28 17:58:13 +0800

    Add files via upload

---
+++ A1103346_0224_2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class A1103346_0224_2{
+  public static void main(String []args){
+
+  float celsius;
+  System.out.println("請輸入任意數直作為攝氏溫度");
+  Scanner sc = new Scanner(System.in);
+  celsius = sc.nextFloat();
+
+  float fahrenheit;
+  fahrenheit = (celsius *9)/ 5 + 32 ;
+  System.out.println("您輸入的攝氏溫度 "+ celsius +" °C等於華氏溫度 "+ fahrenheit +" °F");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
