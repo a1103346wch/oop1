7eb7a72567a45228b72cc73b5e3af8e56b0a1f78
chunghao <125645173+a1103346wch@users.noreply.github.com>
2023-02-28 17:57:48 +0800

    Add files via upload

---
+++ A1103346_0224_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class A1103346_0224_1{
+  public static void main(String []args){
+
+  int num;
+  System.out.println("請輸入任意整數");
+  Scanner sc = new Scanner(System.in);
+  num = sc.nextInt();
+
+  if(num % 2==0){
+    System.out.println("您輸入的整數 "+ num +" 為偶數");}
+  else{
+    System.out.println("您輸入的整數 "+ num +" 為奇數");}
+ 
+ }
+}
