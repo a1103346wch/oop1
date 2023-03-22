a9ba298ca3b4c83a4528f91f15ddc592ba3152ce
chunghao <125645173+a1103346wch@users.noreply.github.com>
2023-03-22 19:58:40 +0800

    Add files via upload

---
+++ A1103346_0317_1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class A1103346_0317_1{
+    public static void main(String []args){
+      int n, m;
+      System.out.println("請輸入任意整數做為n值");
+      Scanner input = new Scanner(System.in);
+      n = input.nextInt();
+      
+      System.out.println("請輸入任意整數做為n值");
+      Scanner in = new Scanner(System.in);
+      m = in.nextInt();
+      System.out.println();
+
+      int[][] array= new int[n][m];
+      for(int i=1; i<=n; i++ ){
+        for(int j=1; j<=m; j++ ){
+            array[i-1][j-1]= i*j;
+        }
+      }
+      
+      
+      for(int []a:array){
+        for(int b:a){
+          System.out.print(b+",");
+        }  
+        System.out.println();
+      } 
+  
+    }
+  }
